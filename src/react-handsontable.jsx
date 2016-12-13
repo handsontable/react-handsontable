862437064d22ac935939819a564a2132b5e75461
Jan Siegel <js.ziggle@gmail.com>
2016-12-13 12:49:29 +0100

    Remove React and Handsontable libraries from the HotTable component (move them only to the demo bundle).

--- src/react-handsontable.jsx
+++ src/react-handsontable.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import moment from 'moment';
-import numbro from 'numbro';
-import pikaday from 'pikaday';
-import Zeroclipboard from 'zeroclipboard';
-import Handsontable from 'handsontable';
 import HotSettingsMapper from './handsontableSettingsMapper';
-require('handsontable/dist/handsontable.full.css');
 
 /**
+ * A Handsontable-ReactJS wrapper.
+ *
+ * To implement, use the `HotTable` tag with properties corresponding to Handsontable options.
+ * For example:
+ *
+ * ```js
+ * <HotTable root="hot" data={dataObject} contextMenu={true} colHeaders={true} width={600} height={300} stretchH="all" />
+ *
+ * // is analogous to
+ * let hot = new Handsontable(document.getElementById('hot'), {
+ *    data: dataObject,
+ *    contextMenu: true,
+ *    colHeaders: true,
+ *    width: 600
+ *    height: 300
+ * });
+ *
+ * ```
+ *
  * @class HotTable
  */
 export default class HotTable extends React.Component {
@@ -19,27 +32,42 @@ export default class HotTable extends React.Component {
     this.root = null;
   }
 
-  //TODO: docs
+  /**
+   * Initialize Handsontable after the component has mounted.
+   */
   componentDidMount() {
     const newSettings = this.hotSettingsMapper.getSettings(this.props);
 
     this.hotInstance = new Handsontable(document.getElementById(this.root), newSettings);
   }
 
-  //TODO: docs
+  /**
+   * Call the `updateHot` method and prevent the component from re-rendering the instance.
+   *
+   * @param {Object} nextProps
+   * @param {Object} nextState
+   * @returns {Boolean}
+   */
   shouldComponentUpdate(nextProps, nextState) {
     this.updateHot(this.hotSettingsMapper.getSettings(nextProps));
 
     return false;
   }
 
-  //TODO: docs
+  /**
+   * Render the table.
+   *
+   * @returns {XML}
+   */
   render() {
     this.root = this.props.root || 'hot' + new Date().getTime();
     return <div id={this.root}></div>
   }
 
-  //TODO: docs
+  /**
+   * Call the `updateSettings` method for the Handsontable instance.
+   * @param newSettings
+   */
   updateHot(newSettings) {
     this.hotInstance.updateSettings(newSettings);
   }
