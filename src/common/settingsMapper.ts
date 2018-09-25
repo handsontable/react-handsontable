358ecdc4187009e4944f2820bbf65150b5381f51
Jan Siegel <jansiegel@users.noreply.github.com>
2018-09-25 12:01:11 +0200

    Remove the `on-` prefixes from the hook declaration. #101 (#102)

--- src/common/settingsMapper.ts
+++ src/common/settingsMapper.ts
@@ -21,35 +21,17 @@ export class SettingsMapper {
       let settings = properties.settings;
       for (const key in settings) {
         if (settings.hasOwnProperty(key)) {
-          newSettings[this.trimHookPrefix(key)] = settings[key];
+          newSettings[key] = settings[key];
         }
       }
     }
 
     for (const key in properties) {
       if (key !== 'settings' && properties.hasOwnProperty(key)) {
-        newSettings[this.trimHookPrefix(key)] = properties[key];
+        newSettings[key] = properties[key];
       }
     }
 
     return newSettings;
-  }
-
-  /**
-   * Trim the "on" hook prefix.
-   *
-   * @param {String} prop Settings property.
-   * @returns {String} Handsontable-compatible, prefix-less property name.
-   */
-  trimHookPrefix(prop: string): string {
-    if (prop.indexOf('on') === 0) {
-      let hookName = prop.charAt(2).toLowerCase() + prop.slice(3, prop.length);
-      if (this.registeredHooks.indexOf(hookName) > -1) {
-        return hookName;
-      }
-    }
-
-    // returns the string anyway, when we're sure all the hooks are registered, might be changed
-    return prop;
   }
 }
