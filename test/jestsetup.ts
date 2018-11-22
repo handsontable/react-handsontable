fe81e220259ebe3b5288ecf39635eb3768ee0f20
Jan Siegel <js.ziggle@gmail.com>
2018-11-22 15:17:23 +0100

    Bump 'jest' to update the 'merge' package because of a security alert. #115

--- test/jestsetup.ts
+++ test/jestsetup.ts
@@ -1,21 +1,9 @@
 import {
-  shallow,
-  render,
   mount,
   configure
 } from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
-import Handsontable from 'handsontable';
 
 configure({adapter: new ReactSixteenAdapter()});
 
-global.shallow = shallow;
-global.render = render;
 global.mount = mount;
-global.Handsontable = Handsontable;
-
-console.error = message => {
-  if (!/(React.createElement: type should not be null)/.test(message)) {
-    throw new Error(message);
-  }
-};
